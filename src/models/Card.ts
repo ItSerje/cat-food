52d6adb1850b6bc1e10454e54d4fafb7cd352c5e
ItSerje <66359825+ItSerje@users.noreply.github.com>
2023-01-19 04:34:56 +0600

    add Card type

---
+++ src/models/Card.ts
@@ -0,0 +1,13 @@
+export interface Card {
+  id: string;
+  productType?: string;
+  productTypeSelectedHover?: string;
+  productTitle: string;
+  productSubtitle?: string;
+  productSpecs?: string[];
+  productWeight?: string;
+  productWeightUnit?: string;
+  cardCtaText?: string;
+  cardCtaTextSelected?: string;
+  cardCtaTextDisabled?: string;
+}
